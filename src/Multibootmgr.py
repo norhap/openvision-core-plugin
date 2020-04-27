8f3937da221e76b62196acc162dc38b3d06de15d
persianpros <persianpros@yahoo.com>
2020-04-27 18:53:10 +0430

    Correct order of the beginning of py files

--- src/Multibootmgr.py
+++ src/Multibootmgr.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 from Components.ActionMap import ActionMap
 from Components.ChoiceList import ChoiceList, ChoiceEntryComponent
--- src/ScriptRunner.py
+++ src/ScriptRunner.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 from . import _, PluginLanguageDomain
 from Screens.Screen import Screen
