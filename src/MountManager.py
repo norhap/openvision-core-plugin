b198340afe3ceb991ac8f17ec8023647ce8daaca
persianpros <persianpros@yahoo.com>
2020-07-01 17:22:32 +0430

    Use python 3 open instead of file

--- src/MountManager.py
+++ src/MountManager.py
@@ -303,11 +303,11 @@ class VISIONDevicesPanel(Screen):
 		# print('[MountManager]add_fstab: device = %s, mountp=%s, UUID=%s' %(self.device, self.mountp, self.device_uuid))
 		if not path.exists(self.mountp):
 			mkdir(self.mountp, 0755)
-		file('/etc/fstab.tmp', 'w').writelines([l for l in file('/etc/fstab').readlines() if '/media/hdd' not in l])
+		file('/etc/fstab.tmp', 'w').writelines([l for l in open('/etc/fstab').readlines() if '/media/hdd' not in l])
 		rename('/etc/fstab.tmp', '/etc/fstab')
-		file('/etc/fstab.tmp', 'w').writelines([l for l in file('/etc/fstab').readlines() if self.device not in l])
+		file('/etc/fstab.tmp', 'w').writelines([l for l in open('/etc/fstab').readlines() if self.device not in l])
 		rename('/etc/fstab.tmp', '/etc/fstab')
-		file('/etc/fstab.tmp', 'w').writelines([l for l in file('/etc/fstab').readlines() if self.device_uuid not in l])
+		file('/etc/fstab.tmp', 'w').writelines([l for l in open('/etc/fstab').readlines() if self.device_uuid not in l])
 		rename('/etc/fstab.tmp', '/etc/fstab')
 		with open('/etc/fstab', 'a') as out:
 			line = self.device_uuid + '\t/media/hdd\tauto\tdefaults\t0 0\n'
@@ -390,9 +390,9 @@ class VISIONDevicePanelConf(Screen, ConfigListScreen):
 
 			if not path.exists(self.mountp):
 				mkdir(self.mountp, 0755)
-			file('/etc/fstab.tmp', 'w').writelines([l for l in file('/etc/fstab').readlines() if self.device not in l])
+			file('/etc/fstab.tmp', 'w').writelines([l for l in open('/etc/fstab').readlines() if self.device not in l])
 			rename('/etc/fstab.tmp', '/etc/fstab')
-			file('/etc/fstab.tmp', 'w').writelines([l for l in file('/etc/fstab').readlines() if self.device_uuid not in l])
+			file('/etc/fstab.tmp', 'w').writelines([l for l in open('/etc/fstab').readlines() if self.device_uuid not in l])
 			rename('/etc/fstab.tmp', '/etc/fstab')
 			with open('/etc/fstab', 'a') as out:
 				line = self.device_uuid + '\t' + self.mountp + '\t' + self.device_type + '\tdefaults\t0 0\n'
