e45466d72328af92447f66057ca9aa316ff0decd
Persian Prince <persianpros@yahoo.com>
2020-10-15 09:10:04 +0330

    Update xml2po.py

--- po/xml2po.py
+++ po/xml2po.py
@@ -3,22 +3,17 @@
 from __future__ import print_function
 import sys
 import os
-import string
+import six
 import re
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler, property_lexical_handler
-
 try:
 	from _xmlplus.sax.saxlib import LexicalHandler
-
 	no_comments = False
-except ImportError as e:
+except ImportError:
 	class LexicalHandler:
-		def __init__(self):
-			pass
-
+		pass
 	no_comments = True
-
 
 class parseXML(ContentHandler, LexicalHandler):
 	def __init__(self, attrlist):
@@ -28,19 +23,18 @@ class parseXML(ContentHandler, LexicalHandler):
 		self.ishex = re.compile('#[0-9a-fA-F]+\Z')
 
 	def comment(self, comment):
-		if comment.find("TRANSLATORS:") != -1:
+		if "TRANSLATORS:" in comment:
 			self.last_comment = comment
 
 	def startElement(self, name, attrs):
 		for x in ["text", "title", "value", "caption", "summary", "description"]:
 			try:
-				k = str(attrs[x])
+				k = six.ensure_str(attrs[x])
 				if k.strip() != "" and not self.ishex.match(k):
 					attrlist.add((attrs[x], self.last_comment))
 					self.last_comment = None
 			except KeyError:
 				pass
-
 
 parser = make_parser()
 
@@ -54,7 +48,7 @@ if not no_comments:
 for arg in sys.argv[1:]:
 	if os.path.isdir(arg):
 		for file in os.listdir(arg):
-			if file.endswith(".xml"):
+			if (file.endswith(".xml")):
 				parser.parse(os.path.join(arg, file))
 	else:
 		parser.parse(arg)
@@ -63,20 +57,13 @@ for arg in sys.argv[1:]:
 	attrlist.sort(key=lambda a: a[0])
 
 	for (k, c) in attrlist:
-		print
+		print()
 		print('#: ' + arg)
-# We need to escape all "s that we have in the string.
-# This replace() *must* come before anything that adds "s!! (such as the
-# newline-handling code).
-# Ideally we'd do this "intelligently" (check for no current preceding \)
-# but there is no reason for there ever to be one, so....
-#
-		k = str(k).replace('"', '\\"')
-		k = k.replace("\\n", "\"\n\"")
+		k.replace("\\n", "\"\n\"")
 		if c:
 			for l in c.split('\n'):
 				print("#. ", l)
-		print('msgid "' + k + '"')
+		print('msgid "' + six.ensure_str(k) + '"')
 		print('msgstr ""')
 
 	attrlist = set()
