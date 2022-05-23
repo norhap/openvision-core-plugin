34fc3c17f0dc035187fcf39bcc4ac119b03b30c1
norhap <elfogondesegovia@gmail.com>
2022-05-23 16:02:13 +0200

    fix check ov boot PEP

--- CI/build.sh
+++ CI/build.sh
@@ -21,7 +21,7 @@ commit_files() {
 }
 
 upload_files() {
-  git remote add upstream https://${GITHUB_TOKEN}@github.com/OpenVisionE2/openvision-core-plugin.git > /dev/null 2>&1
+  git remote add upstream https://${GITHUB_TOKEN}@github.com/norhap/openvision-core-plugin.git > /dev/null 2>&1
   git push --quiet upstream master || echo "failed to push with error $?"
 }
 
